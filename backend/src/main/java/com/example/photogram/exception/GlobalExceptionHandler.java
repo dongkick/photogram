e0bfcd2b7bee925786667bb432db38785a0de0ca
hr9919 <hrnam99@chungbuk.ac.kr>
2024-12-01 11:45:12 +0900

    Update GlobalExceptionHandler.java

--- backend/src/main/java/com/example/photogram/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/photogram/exception/GlobalExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.example.photogram.exception;
 
-import com.example.photogram.exception.UsernameAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 
 @CrossOrigin(origins = "http://localhost:3000") // 클라이언트 도메인 설정
 @RestControllerAdvice
@@ -15,5 +13,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<String> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 존재하는 ID입니다.");
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity<String> handleInvalidPassword(InvalidPasswordException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 비밀번호입니다.");
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("사용자를 찾을 수 없습니다.");
     }
 }
